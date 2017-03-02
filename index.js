fa1cfababeafb0120c62f27eb9469ba9e4804103
Zach Burmeister <zburmeister@hanekedesign.com>
2017-03-02 16:38:21 -0700

    port changed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
-app.listen(5000, () => {
-	console.log('running on port 3050');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
